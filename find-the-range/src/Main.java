60616f2b373fb3eba7aa61d5bd7bfb942b9559a1
Rizve Rahman Reza <rizverahman.ai@outlook.com>
2024-08-16 19:07:26 +0600

    commit

---
+++ find-the-range/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 5;
+        int y = 20;
+        int p = 3;
+
+        System.out.println(y/p - x/p);
+    }
+}
---
+++ sumOfPrimeNum/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int i, sum = 0;
+
+        for(i = 0; i <= 100; i++){
+            if (i%2 == 0){
+                sum = sum + i;
+                System.out.println("Sum of first 100 odd");
+            }
+
+        }
+    }
+}
